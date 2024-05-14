69725b2c93e4ff183a79185c5469156f0cd9de99
hojat <hojat72elect@gmail.com>
2024-05-13 20:35:44 -0400

    DesktopLauncher.java -> kt

--- libgdx_new_super_jumper/desktop/src/com/nopalsoft/superjumper/desktop/DesktopLauncher.kt
+++ libgdx_new_super_jumper/desktop/src/com/nopalsoft/superjumper/desktop/DesktopLauncher.kt
@@ -1,16 +1,13 @@
-package com.nopalsoft.superjumper.desktop;
+package com.nopalsoft.superjumper.desktop
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.nopalsoft.superjumper.MainSuperJumper;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.nopalsoft.superjumper.MainSuperJumper
 
 
-
-public class DesktopLauncher {
-
-    public static void main(String[] arg) {
-        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setWindowedMode(480, 800);
-        new Lwjgl3Application(new MainSuperJumper(), config);
-    }
+fun main() {
+    val config = Lwjgl3ApplicationConfiguration()
+    config.setWindowedMode(480, 800)
+    Lwjgl3Application(MainSuperJumper(), config)
 }
+
