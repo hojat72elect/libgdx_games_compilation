12150613752e3229d2d1d490fec186bbd9d84caa
hojat <hojat72elect@gmail.com>
2024-05-14 14:30:59 -0400

    Migrated desktop module to kotlin.

--- libgdx_galaxy_invaders/desktop/src/com/nopalsoft/invaders/desktop/DesktopLauncher.kt
+++ libgdx_galaxy_invaders/desktop/src/com/nopalsoft/invaders/desktop/DesktopLauncher.kt
@@ -1,13 +1,12 @@
-package com.nopalsoft.invaders.desktop;
+package com.nopalsoft.invaders.desktop
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.nopalsoft.invaders.MainInvaders;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.nopalsoft.invaders.MainInvaders
 
-public class DesktopLauncher {
-    public static void main(String[] args) {
-        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setWindowedMode(480, 800);
-        new Lwjgl3Application(new MainInvaders(), config);
-    }
+fun main() {
+    val config = Lwjgl3ApplicationConfiguration()
+    config.setWindowedMode(480, 800)
+    Lwjgl3Application(MainInvaders(), config)
 }
+
