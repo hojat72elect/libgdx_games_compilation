ed36847f4496c6617a87e8ed657404bce87befb0
hojat <hojat72elect@gmail.com>
2024-05-14 14:35:49 -0400

    Migrated Android module to kotlin.

--- libgdx_galaxy_invaders/android/src/com/nopalsoft/invaders/AndroidLauncher.kt
+++ libgdx_galaxy_invaders/android/src/com/nopalsoft/invaders/AndroidLauncher.kt
@@ -1,19 +1,17 @@
-package com.nopalsoft.invaders;
+package com.nopalsoft.invaders
 
-import android.os.Bundle;
+import android.os.Bundle
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.nopalsoft.invaders.MainInvaders;
+class AndroidLauncher : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val config = AndroidApplicationConfiguration()
+        initialize(MainInvaders(), config)
+    }
 
-public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MainInvaders(), config);
-	}
-
-	@Override
-	public void onBackPressed() { }
+    override fun onBackPressed() {
+        // todo
+    }
 }
