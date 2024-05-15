d363ce4c644b6a17cdb15625d39253254ad434c6
hojat <hojat72elect@gmail.com>
2024-05-15 01:36:35 -0400

    Removed HTML module from ninja runner game.

--- libgdx_ninja_runner/desktop/src/com/nopalsoft/ninjarunner/desktop/DesktopLauncher.java
+++ libgdx_ninja_runner/desktop/src/com/nopalsoft/ninjarunner/desktop/DesktopLauncher.java
@@ -13,65 +13,6 @@ import com.nopalsoft.ninjarunner.leaderboard.Person;
 public class DesktopLauncher {
 
     public static MainGame game;
-    private static boolean isSigned = true;
-
-    public static void main(String[] arg) {
-        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setWindowedMode(800, 480);
-        config.setTitle("Ninja Runner");
-        game = new MainGame(handler, gameHandler, faceHandler);
-        new Lwjgl3Application(game, config);
-    }
-
-    static FacebookHandler faceHandler = new FacebookHandler() {
-
-        @Override
-        public void facebookSignOut() {
-            isSigned = false;
-            Gdx.app.log("Facebook", "Sign out");
-        }
-
-        @Override
-        public void facebookSignIn() {
-            isSigned = true;
-            Gdx.app.log("Facebook", "Sign in");
-            facebookGetScores();
-        }
-
-
-        @Override
-        public boolean facebookIsSignedIn() {
-            return isSigned;
-        }
-
-        @Override
-        public void facebookGetScores() {
-            String imageURL = "https://randomuser.me/api/portraits/men/";
-            Array<Person> arrPerson = new Array<>();
-            arrPerson.add(new Person(Person.TipoCuenta.FACEBOOK, "1", "Jessie", 15000, imageURL + "18.jpg"));
-            arrPerson.add(new Person(Person.TipoCuenta.FACEBOOK, "2", "Rogelio", 10000, imageURL + "17.jpg"));
-            arrPerson.add(new Person(Person.TipoCuenta.FACEBOOK, "3", "Susana", 8000, imageURL + "16.jpg"));
-            arrPerson.add(new Person(Person.TipoCuenta.FACEBOOK, "4", "Flavia", 5000, imageURL + "15.jpg"));
-            arrPerson.add(new Person(Person.TipoCuenta.FACEBOOK, "5", "Micky", 2500, imageURL + "14.jpg"));
-            arrPerson.add(new Person(Person.TipoCuenta.FACEBOOK, "6", "Carlos", 1000, imageURL + "13.jpg"));
-            Person yayo = new Person(Person.TipoCuenta.FACEBOOK, "7", "Yayo", 0, imageURL + "12.jpg");
-            yayo.isMe = true;
-            arrPerson.add(yayo);
-            game.setArrayPerson(arrPerson);
-        }
-
-        @Override
-        public void facebookSubmitScore(long score) {
-
-        }
-
-        @Override
-        public void facebookInviteFriends(String message) {
-            // TODO Auto-generated method stub
-
-        }
-    };
-
     static GoogleGameServicesHandler gameHandler = new GoogleGameServicesHandler() {
 
         @Override
@@ -121,7 +62,6 @@ public class DesktopLauncher {
 
         }
     };
-
     static RequestHandler handler = new RequestHandler() {
 
         @Override
@@ -196,4 +136,61 @@ public class DesktopLauncher {
 
         }
     };
+    private static boolean isSigned = true;
+    static FacebookHandler faceHandler = new FacebookHandler() {
+
+        @Override
+        public void facebookSignOut() {
+            isSigned = false;
+            Gdx.app.log("Facebook", "Sign out");
+        }
+
+        @Override
+        public void facebookSignIn() {
+            isSigned = true;
+            Gdx.app.log("Facebook", "Sign in");
+            facebookGetScores();
+        }
+
+
+        @Override
+        public boolean facebookIsSignedIn() {
+            return isSigned;
+        }
+
+        @Override
+        public void facebookGetScores() {
+            String imageURL = "https://randomuser.me/api/portraits/men/";
+            Array<Person> arrPerson = new Array<>();
+            arrPerson.add(new Person(Person.TipoCuenta.FACEBOOK, "1", "Jessie", 15000, imageURL + "18.jpg"));
+            arrPerson.add(new Person(Person.TipoCuenta.FACEBOOK, "2", "Rogelio", 10000, imageURL + "17.jpg"));
+            arrPerson.add(new Person(Person.TipoCuenta.FACEBOOK, "3", "Susana", 8000, imageURL + "16.jpg"));
+            arrPerson.add(new Person(Person.TipoCuenta.FACEBOOK, "4", "Flavia", 5000, imageURL + "15.jpg"));
+            arrPerson.add(new Person(Person.TipoCuenta.FACEBOOK, "5", "Micky", 2500, imageURL + "14.jpg"));
+            arrPerson.add(new Person(Person.TipoCuenta.FACEBOOK, "6", "Carlos", 1000, imageURL + "13.jpg"));
+            Person yayo = new Person(Person.TipoCuenta.FACEBOOK, "7", "Yayo", 0, imageURL + "12.jpg");
+            yayo.isMe = true;
+            arrPerson.add(yayo);
+            game.setArrayPerson(arrPerson);
+        }
+
+        @Override
+        public void facebookSubmitScore(long score) {
+
+        }
+
+        @Override
+        public void facebookInviteFriends(String message) {
+            // TODO Auto-generated method stub
+
+        }
+    };
+
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setWindowedMode(800, 480);
+        config.setTitle("Ninja Runner");
+        game = new MainGame(handler, gameHandler, faceHandler);
+        new Lwjgl3Application(game, config);
+    }
 }
