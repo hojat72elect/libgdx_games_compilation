ee3ff8c35b31c7e13df858526b7fa161484c9c70
hojat <hojat72elect@gmail.com>
2024-05-16 00:00:00 -0400

    Migrated the Android app to kt.

--- libgdx_sokoban/android/src/com/nopalsoft/sokoban/AndroidLauncher.kt
+++ libgdx_sokoban/android/src/com/nopalsoft/sokoban/AndroidLauncher.kt
@@ -1,15 +1,13 @@
-package com.nopalsoft.sokoban;
+package com.nopalsoft.sokoban
 
-import android.os.Bundle;
+import android.os.Bundle
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-
-public class AndroidLauncher extends AndroidApplication {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-        initialize(new MainSokoban(), config);
+class AndroidLauncher : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val config = AndroidApplicationConfiguration()
+        initialize(MainSokoban(), config)
     }
 }
