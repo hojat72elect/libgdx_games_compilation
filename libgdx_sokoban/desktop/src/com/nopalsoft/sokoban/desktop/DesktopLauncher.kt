724684a315c170dc51c434e33f0dd160f17ac660
hojat <hojat72elect@gmail.com>
2024-05-15 23:51:55 -0400

    Started migrating sokoban to kotlin.

--- libgdx_sokoban/desktop/src/com/nopalsoft/sokoban/desktop/DesktopLauncher.kt
+++ libgdx_sokoban/desktop/src/com/nopalsoft/sokoban/desktop/DesktopLauncher.kt
@@ -1,14 +1,13 @@
-package com.nopalsoft.sokoban.desktop;
+package com.nopalsoft.sokoban.desktop
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.nopalsoft.sokoban.MainSokoban;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.nopalsoft.sokoban.MainSokoban
 
-public class DesktopLauncher {
-    public static void main(String[] arg) {
-        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setTitle("Sokoban");
-        config.setWindowedMode(800, 480);
-        new Lwjgl3Application(new MainSokoban(), config);
-    }
+
+fun main() {
+    val config = Lwjgl3ApplicationConfiguration()
+    config.setTitle("Sokoban")
+    config.setWindowedMode(800, 480)
+    Lwjgl3Application(MainSokoban(), config)
 }
