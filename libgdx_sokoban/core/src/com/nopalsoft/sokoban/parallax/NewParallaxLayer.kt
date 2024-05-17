b95361a40462c987887d6ffa825f2303090364df
hojat <hojat72elect@gmail.com>
2024-05-17 19:37:26 -0400

    Created a kt version of ParallaxLayer.java

---
+++ libgdx_sokoban/core/src/com/nopalsoft/sokoban/parallax/NewParallaxLayer.kt
@@ -0,0 +1,19 @@
+package com.nopalsoft.sokoban.parallax
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.math.Vector2
+
+/**
+ * @param region        The TextureRegion to draw, this can of be any width/height.
+ * @param parallaxRatio The relative speed of x,y {@link ParallaxBackground#ParallaxBackground(ParallaxLayer[], float, float, Vector2)}.
+ * @param startPosition The initial position of x, y.
+ * @param padding       The padding of the region at x, y.
+ */
+data class NewParallaxLayer(
+    val region: TextureRegion,
+    val parallaxRatio: Vector2,
+    val startPosition: Vector2,
+    val padding: Vector2,
+    val width: Float,
+    val height: Float,
+)
