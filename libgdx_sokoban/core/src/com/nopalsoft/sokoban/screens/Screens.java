a80b0ca97c35a54a1ecd051ae70b185de8e3f79a
hojat <hojat72elect@gmail.com>
2024-05-18 02:08:20 -0400

    Cleaned a bit.

--- libgdx_sokoban/core/src/com/nopalsoft/sokoban/screens/Screens.java
+++ libgdx_sokoban/core/src/com/nopalsoft/sokoban/screens/Screens.java
@@ -142,19 +142,16 @@ public abstract class Screens extends InputAdapter implements Screen, GestureLis
 
     @Override
     public boolean touchDown(float x, float y, int pointer, int button) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean tap(float x, float y, int count, int button) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean longPress(float x, float y) {
-        // TODO Auto-generated method stub
         return false;
     }
 
@@ -178,25 +175,21 @@ public abstract class Screens extends InputAdapter implements Screen, GestureLis
 
     @Override
     public boolean pan(float x, float y, float deltaX, float deltaY) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean panStop(float x, float y, int pointer, int button) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean zoom(float initialDistance, float distance) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
-        // TODO Auto-generated method stub
         return false;
     }
 
