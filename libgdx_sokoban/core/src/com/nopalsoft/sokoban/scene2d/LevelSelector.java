6f3dabda6878d1d92b81684b43c3a89ad0d6a6d0
hojat <hojat72elect@gmail.com>
2024-05-18 01:24:53 -0400

    Cleaned a bit.

--- libgdx_sokoban/core/src/com/nopalsoft/sokoban/scene2d/LevelSelector.java
+++ libgdx_sokoban/core/src/com/nopalsoft/sokoban/scene2d/LevelSelector.java
@@ -12,18 +12,14 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.I18NBundle;
 import com.nopalsoft.sokoban.Assets;
-import com.nopalsoft.sokoban.MainSokoban;
 import com.nopalsoft.sokoban.Settings;
 import com.nopalsoft.sokoban.screens.MainMenuScreen;
 import com.nopalsoft.sokoban.screens.Screens;
 
 public class LevelSelector extends Group {
 
-    protected MainMenuScreen menuScreen;
-    protected I18NBundle languages;
-    protected MainSokoban game;
+    private final MainMenuScreen menuScreen;
 
     Label labelTitle;
 
@@ -40,8 +36,6 @@ public class LevelSelector extends Group {
         setSize(600, 385);
         setPosition(Screens.SCREEN_WIDTH / 2f - getWidth() / 2f, 70);
         menuScreen = (MainMenuScreen) currentScreen;
-        game = currentScreen.game;
-        languages = game.languages;
 
         setBackGround(Assets.backgroundWindow);
 
