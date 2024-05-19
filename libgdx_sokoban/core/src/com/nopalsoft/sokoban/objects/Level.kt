ee1bab073e960864e4701dc8d5bd62357218c9e7
hojat <hojat72elect@gmail.com>
2024-05-18 21:46:15 -0400

    Added docs.

--- libgdx_sokoban/core/src/com/nopalsoft/sokoban/objects/Level.kt
+++ libgdx_sokoban/core/src/com/nopalsoft/sokoban/objects/Level.kt
@@ -1,5 +1,11 @@
 package com.nopalsoft.sokoban.objects
 
+/**
+ * Represents each level of the game that we have played.
+ * @param numStars Maximum number of stars that user has gained in this level.
+ * @param bestTime The least amount of seconds that user has taken for completing this level.
+ * @param bestMoves The least number of moves user has so far taken for solving this level.
+ */
 data class Level(
     var numStars: Int = 0,
     var bestTime: Int = 0,
