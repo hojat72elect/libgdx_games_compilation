baa1914e50bd52fa9f3de3402ee99f105b9c048f
hojat <hojat72elect@gmail.com>
2024-05-26 16:58:29 -0400

    AnimatedSpriteActor.java -> kt

--- libgdx_ninja_runner/core/src/com/nopalsoft/ninjarunner/scene2d/AnimatedSpriteActor.kt
+++ libgdx_ninja_runner/core/src/com/nopalsoft/ninjarunner/scene2d/AnimatedSpriteActor.kt
@@ -1,31 +1,22 @@
-package com.nopalsoft.ninjarunner.scene2d;
+package com.nopalsoft.ninjarunner.scene2d
 
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.nopalsoft.ninjarunner.AnimationSprite;
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.scenes.scene2d.Actor
+import com.nopalsoft.ninjarunner.AnimationSprite
 
+class AnimatedSpriteActor(val animation: AnimationSprite) : Actor() {
+    private var stateTime = 0f
 
-public class AnimatedSpriteActor extends Actor {
-    AnimationSprite anim;
-    float stateTime;
-
-    public AnimatedSpriteActor(AnimationSprite animation) {
-        anim = animation;
-        stateTime = 0;
+    override fun act(delta: Float) {
+        super.act(delta)
+        stateTime += delta
     }
 
-    @Override
-    public void act(float delta) {
-        super.act(delta);
-        stateTime += delta;
-    }
-
-    @Override
-    public void draw(Batch batch, float parentAlpha) {
-        Sprite sprite = anim.getKeyFrame(stateTime, true);
-        sprite.setSize(getWidth(), getHeight());
-        sprite.setPosition(getX(), getY());
-        sprite.draw(batch);
+    override fun draw(batch: Batch?, parentAlpha: Float) {
+        val sprite: Sprite = animation.getKeyFrame(stateTime, true)
+        sprite.setSize(width, height)
+        sprite.setPosition(x, y)
+        sprite.draw(batch)
     }
 }
