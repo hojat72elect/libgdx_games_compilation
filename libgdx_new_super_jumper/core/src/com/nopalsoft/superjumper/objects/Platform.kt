7e6e8da046f8194713a2e692ea5ff82eba34a497
hojat <hojat72elect@gmail.com>
2024-05-28 19:22:42 -0400

    Platform.java -> kt

--- libgdx_new_super_jumper/core/src/com/nopalsoft/superjumper/objects/Platform.kt
+++ libgdx_new_super_jumper/core/src/com/nopalsoft/superjumper/objects/Platform.kt
@@ -1,74 +1,80 @@
-package com.nopalsoft.superjumper.objects;
+package com.nopalsoft.superjumper.objects
 
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Pool.Poolable;
+import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.utils.Pool.Poolable
 
-public class Platform implements Poolable {
-	public final static int STATE_NORMAL = 0;
-	public final static int STATE_DESTROY = 1;
-	public int state;
+class Platform : Poolable {
 
-	public static final int TYPE_NORMAL = 0;
-	public static final int TYPE_BREAKABLE = 1;
-	public int type;
+    @JvmField
+    var state = 0
 
-	public static final float DRAW_WIDTH_NORMAL = 1.25f;
-	public static final float DRAW_HEIGHT_NORMAL = .45f;
-	public static final float WIDTH_NORMAL = 1.25f;
-	public static final float HEIGHT_NORMAL = .45f;
+    @JvmField
+    var type = 0
 
-	public static final int COLOR_BEIGE = 0;
-	public static final int COLOR_BLUE = 1;
-	public static final int COLOR_GRAY = 2;
-	public static final int COLOR_GREEN = 3;
-	public static final int COLOR_MULTICOLOR = 4;
-	public static final int COLOR_PINK = 5;
-	public static final int COLOR_BEIGE_LIGHT = 6;
-	public static final int COLOR_BLUE_LIGHT = 7;
-	public static final int COLOR_GRAY_LIGHT = 8;
-	public static final int COLOR_GREEN_LIGHT = 9;
-	public static final int COLOR_MULTICOLOR_LIGHT = 10;
-	public static final int COLOR_PINK_LIGHT = 11;
-	public int color;
+    @JvmField
+    var color = 0
 
-	public final Vector2 position;
-	public float stateTime;
+    @JvmField
+    val position = Vector2()
+    var stateTime = 0f
 
-	public Platform() {
-		position = new Vector2();
 
-	}
+    fun initializePlatform(x: Float, y: Float, type: Int) {
+        position.set(x, y)
+        this.type = type
 
-	public void initializePlatform(float x, float y, int type) {
-		position.set(x, y);
-		this.type = type;
+        color = if (type == TYPE_NORMAL) {
+            MathUtils.random(11)
+        } else {
+            MathUtils.random(5)
+        }
+        state = STATE_NORMAL
+        stateTime = 0f
+    }
 
-		if (type == TYPE_NORMAL) {
-			color = MathUtils.random(11);
-		}
-		else {
-			color = MathUtils.random(5);
-		}
-		state = STATE_NORMAL;
-		stateTime = 0;
-	}
 
-	public void update(float delta) {
-		stateTime += delta;
-	}
+    fun update(delta: Float) {
+        stateTime += delta
+    }
 
-	public void setDestroy() {
-		if (state == STATE_NORMAL) {
-			state = STATE_DESTROY;
-			stateTime = 0;
-		}
-	}
 
-	@Override
-	public void reset() {
-		// TODO Auto-generated method stub
+    fun setDestroy() {
+        if (state == STATE_NORMAL) {
+            state = STATE_DESTROY
+            stateTime = 0f
+        }
+    }
 
-	}
 
+    override fun reset() {
+        // Nothing is happening in here.
+    }
+
+    companion object {
+
+        const val STATE_NORMAL = 0
+        const val STATE_DESTROY = 1
+
+        const val TYPE_NORMAL = 0
+        const val TYPE_BREAKABLE = 1
+
+        const val DRAW_WIDTH_NORMAL = 1.25f
+        const val DRAW_HEIGHT_NORMAL = .45f
+        const val WIDTH_NORMAL = 1.25f
+        const val HEIGHT_NORMAL = .45f
+
+        const val COLOR_BEIGE = 0
+        const val COLOR_BLUE = 1
+        const val COLOR_GRAY = 2
+        const val COLOR_GREEN = 3
+        const val COLOR_MULTICOLOR = 4
+        const val COLOR_PINK = 5
+        const val COLOR_BEIGE_LIGHT = 6
+        const val COLOR_BLUE_LIGHT = 7
+        const val COLOR_GRAY_LIGHT = 8
+        const val COLOR_GREEN_LIGHT = 9
+        const val COLOR_MULTICOLOR_LIGHT = 10
+        const val COLOR_PINK_LIGHT = 11
+    }
 }
