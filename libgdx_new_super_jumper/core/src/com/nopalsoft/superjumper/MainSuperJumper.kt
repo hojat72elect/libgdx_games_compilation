6849722dd0f79e564bafb252dfb29192d1e686f5
hojat <hojat72elect@gmail.com>
2024-06-02 21:10:00 -0400

    MainSuperJumper.java -> kt

--- libgdx_new_super_jumper/core/src/com/nopalsoft/superjumper/MainSuperJumper.kt
+++ libgdx_new_super_jumper/core/src/com/nopalsoft/superjumper/MainSuperJumper.kt
@@ -1,29 +1,21 @@
-package com.nopalsoft.superjumper;
+package com.nopalsoft.superjumper
 
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.nopalsoft.superjumper.screens.BasicScreen;
-import com.nopalsoft.superjumper.screens.MainMenuScreen;
+import com.badlogic.gdx.Game
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.utils.viewport.StretchViewport
+import com.nopalsoft.superjumper.screens.BasicScreen
+import com.nopalsoft.superjumper.screens.MainMenuScreen
 
-public class MainSuperJumper extends Game {
+class MainSuperJumper : Game() {
+    lateinit var stage: Stage
+    lateinit var batcher: SpriteBatch
 
-    public Stage stage;
-    public SpriteBatch batcher;
-    public MainSuperJumper() {
-
-    }
-
-    @Override
-    public void create() {
-
-        stage = new Stage(new StretchViewport(BasicScreen.SCREEN_WIDTH, BasicScreen.SCREEN_HEIGHT));
-
-        batcher = new SpriteBatch();
-        Settings.load();
-        Assets.load();
-
-        setScreen(new MainMenuScreen(this));
+    override fun create() {
+        stage = Stage(StretchViewport(BasicScreen.SCREEN_WIDTH, BasicScreen.SCREEN_HEIGHT))
+        batcher = SpriteBatch()
+        Settings.load()
+        Assets.load()
+        setScreen(MainMenuScreen(this))
     }
 }
