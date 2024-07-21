26967f8ea83964283a65d086db847b975d41c360
hojat <hojat72elect@gmail.com>
2024-07-21 02:04:05 -0400

    Screens.java -> KT

--- libgdx_galaxy_invaders/core/src/com/nopalsoft/invaders/screens/Screens.kt
+++ libgdx_galaxy_invaders/core/src/com/nopalsoft/invaders/screens/Screens.kt
@@ -1,113 +1,109 @@
-package com.nopalsoft.invaders.screens;
+package com.nopalsoft.invaders.screens
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.InputMultiplexer;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.nopalsoft.invaders.Assets;
-import com.nopalsoft.invaders.MainInvaders;
-import com.nopalsoft.invaders.game.GameScreen;
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputAdapter
+import com.badlogic.gdx.InputMultiplexer
+import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.nopalsoft.invaders.Assets
+import com.nopalsoft.invaders.MainInvaders
+import com.nopalsoft.invaders.Settings
+import com.nopalsoft.invaders.game.GameScreen
 
-public abstract class Screens extends InputAdapter implements Screen {
-    public static final int SCREEN_WIDTH = 320;
-    public static final int SCREEN_HEIGHT = 480;
+abstract class Screens(@JvmField val game: MainInvaders) : InputAdapter(), Screen {
 
-    public static final int WORLD_SCREEN_WIDTH = 32;
-    public static final int WORLD_SCREEN_HEIGHT = 48;
+    @JvmField
+    val myCamera = OrthographicCamera(SCREEN_WIDTH.toFloat(), SCREEN_HEIGHT.toFloat())
 
-    public MainInvaders game;
+    @JvmField
+    val batcher = game.spriteBatch
 
-    public OrthographicCamera myCamera;
-    public SpriteBatch batcher;
-    public Stage stage;
-    public Assets myAssets;
+    @JvmField
+    val stage = game.stage
 
-    public Screens(MainInvaders game) {
-        stage = game.stage;
-        stage.clear();
-        this.game = game;
-        myAssets = game.oAssets;
+    init {
+        stage?.clear()
+        myCamera.position.set(SCREEN_WIDTH / 2f, SCREEN_HEIGHT / 2f, 0f)
+        val input = InputMultiplexer(this, stage)
+        Gdx.input.inputProcessor = input
 
-        myCamera = new OrthographicCamera(SCREEN_WIDTH, SCREEN_HEIGHT);
-        myCamera.position.set(SCREEN_WIDTH / 2f, SCREEN_HEIGHT / 2f, 0);
-        batcher = game.spriteBatch;
+        Assets.font10.data.setScale(.65f)
+        Assets.font15.data.setScale(1f)
+        Assets.font45.data.setScale(.85f)
+        Assets.font60.data.setScale(1.2f)
 
-        InputMultiplexer input = new InputMultiplexer(this, stage);
-        Gdx.input.setInputProcessor(input);
+        when (this) {
+            is MainMenuScreen -> {
+                Assets.font10.data.setScale(.65f)
+                Assets.font15.data.setScale(1f)
+                Assets.font45.data.setScale(.85f)
+                Assets.font60.data.setScale(1.2f)
+            }
 
-        Assets.font10.getData().setScale(.65f);
-        Assets.font15.getData().setScale(1f);
-        Assets.font45.getData().setScale(.85f);
-        Assets.font60.getData().setScale(1.2f);
-        if (this instanceof MainMenuScreen) {
-            Assets.font10.getData().setScale(.65f);
-            Assets.font15.getData().setScale(1f);
-            Assets.font45.getData().setScale(.85f);
-            Assets.font60.getData().setScale(1.2f);
-        } else if (this instanceof GameScreen) {
-            Assets.font15.getData().setScale(1);
-            Assets.font45.getData().setScale(.7f);
-            Assets.font10.getData().setScale(.65f);
-        } else if (this instanceof SettingsScreen) {
-            Assets.font10.getData().setScale(1);
-            Assets.font15.getData().setScale(1f);
-            Assets.font45.getData().setScale(.65f);
-            Assets.font60.getData().setScale(1);
+            is GameScreen -> {
+                Assets.font15.data.setScale(1f)
+                Assets.font45.data.setScale(.7f)
+                Assets.font10.data.setScale(.65f)
+            }
+
+            is SettingsScreen -> {
+                Assets.font10.data.setScale(1f)
+                Assets.font15.data.setScale(1f)
+                Assets.font45.data.setScale(.65f)
+                Assets.font60.data.setScale(1f)
+            }
         }
 
-    }
-
-    @Override
-    public void render(float delta) {
-        update(delta);
-
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        draw(delta);
-
-        stage.act(delta);
-        stage.draw();
-    }
-
-    public abstract void draw(float delta);
-
-    public abstract void update(float delta);
-
-    @Override
-    public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true);
-    }
-
-    @Override
-    public void show() {
 
     }
 
-    @Override
-    public void hide() {
-        com.nopalsoft.invaders.Settings.save();
+    override fun render(delta: Float) {
+        update(delta)
+
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        draw(delta)
+
+        stage?.act(delta)
+        stage?.draw()
     }
 
-    @Override
-    public void pause() {
-        Assets.music.pause();
+    abstract fun draw(delta: Float)
+
+    abstract fun update(delta: Float)
+
+    override fun resize(width: Int, height: Int) {
+        stage?.viewport?.update(width, height, true)
     }
 
-    @Override
-    public void resume() {
-        if (com.nopalsoft.invaders.Settings.getMusicEnabled() && !Assets.music.isPlaying())
-            Assets.music.play();
+    override fun show() {
     }
 
-    @Override
-    public void dispose() {
-        stage.dispose();
-        batcher.dispose();
+    override fun hide() {
+        Settings.save()
     }
 
+    override fun pause() {
+        Assets.music.pause()
+    }
+
+    override fun resume() {
+        if (Settings.musicEnabled && Assets.music.isPlaying.not()) Assets.music.play()
+    }
+
+    override fun dispose() {
+        stage?.dispose()
+        batcher?.dispose()
+    }
+
+
+    companion object {
+        const val SCREEN_WIDTH = 320
+        const val SCREEN_HEIGHT = 480
+
+        const val WORLD_SCREEN_WIDTH = 32
+        const val WORLD_SCREEN_HEIGHT = 48
+    }
 }
