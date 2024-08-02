5e9e8c583be04d9f78a53fce0793f39123599f2e
hojat <hojat72elect@gmail.com>
2024-08-01 23:43:57 -0400

    LeaderboardScreen.java -> kt

--- libgdx_ninja_runner/core/src/com/nopalsoft/ninjarunner/leaderboard/LeaderboardScreen.kt
+++ libgdx_ninja_runner/core/src/com/nopalsoft/ninjarunner/leaderboard/LeaderboardScreen.kt
@@ -1,152 +1,137 @@
-package com.nopalsoft.ninjarunner.leaderboard;
+package com.nopalsoft.ninjarunner.leaderboard
 
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
-import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane.ScrollPaneStyle;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.nopalsoft.ninjarunner.Assets;
-import com.nopalsoft.ninjarunner.game.GameScreen;
-import com.nopalsoft.ninjarunner.screens.Screens;
+import com.badlogic.gdx.Game
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.scenes.scene2d.ui.*
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane.ScrollPaneStyle
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.nopalsoft.ninjarunner.Assets
+import com.nopalsoft.ninjarunner.game.GameScreen
+import com.nopalsoft.ninjarunner.screens.Screens
 
 /**
  * Shows all the top scoring users of the game. It doesn't matter which social network they have
  * used for logging in to the game; here they are shown if they are leader board users of the game.
  */
-public class LeaderboardScreen extends Screens {
+class LeaderboardScreen(_game: Game?) : Screens(_game) {
+    var tableMenu: Table
+    var buttonLeaderboard: Button? = null
+    var buttonFacebook: Button? = null
+    var buttonInviteFriend: Button? = null
+    var buttonGoogle: Button? = null
 
-    Table tableMenu;
-    Button buttonLeaderboard, buttonFacebook, buttonInviteFriend;
-    Button buttonGoogle;
-
-    ScrollPane scroll;
-    Table table;
-
-
-    public LeaderboardScreen(Game _game) {
-        super(_game);
-
-        Label labelShop = new Label("Leaderboards", Assets.labelStyleLarge);
-
-        Table tableTitle = new Table();
-        tableTitle.setSize(400, 100);
-        tableTitle.setPosition(SCREEN_WIDTH / 2f - tableTitle.getWidth() / 2f, SCREEN_HEIGHT - tableTitle.getHeight());
-        tableTitle.setBackground(Assets.backgroundTitleShop);
-        tableTitle.padTop(20).padBottom(5);
-
-        tableTitle.row().colspan(2);
-        tableTitle.add(labelShop).expand();
-        tableTitle.row();
-
-        Image imgGem = new Image(Assets.coinAnimation.getKeyFrame(0));
-        imgGem.setSize(20, 20);
+    var scroll: ScrollPane
+    var table: Table
 
 
-        initButtons();
+    init {
+        val labelShop = Label("Leaderboards", Assets.labelStyleLarge)
 
-        tableMenu = new Table();
-        tableMenu.defaults().size(58).padBottom(8);
+        val tableTitle = Table()
+        tableTitle.setSize(400f, 100f)
+        tableTitle.setPosition(SCREEN_WIDTH / 2f - tableTitle.width / 2f, SCREEN_HEIGHT - tableTitle.height)
+        tableTitle.background = Assets.backgroundTitleShop
+        tableTitle.padTop(20f).padBottom(5f)
 
-        tableMenu.row();
-        tableMenu.add(buttonLeaderboard);
+        tableTitle.row().colspan(2)
+        tableTitle.add(labelShop).expand()
+        tableTitle.row()
 
-        tableMenu.row();
-        tableMenu.add(buttonFacebook);
-
-        tableMenu.row();
-        tableMenu.add(buttonGoogle);
-
-        tableMenu.row();
-        tableMenu.add(buttonInviteFriend);
+        val imgGem = Image(Assets.coinAnimation.getKeyFrame(0f))
+        imgGem.setSize(20f, 20f)
 
 
-        Table tbShop = new Table();
-        tbShop.setSize(SCREEN_WIDTH, SCREEN_HEIGHT - tableTitle.getHeight());
-        tbShop.setBackground(Assets.backgroundShop);
-        tbShop.pad(25, 5, 15, 5);
+        initButtons()
+
+        tableMenu = Table()
+        tableMenu.defaults().size(58f).padBottom(8f)
+
+        tableMenu.row()
+        tableMenu.add(buttonLeaderboard)
+
+        tableMenu.row()
+        tableMenu.add(buttonFacebook)
+
+        tableMenu.row()
+        tableMenu.add(buttonGoogle)
+
+        tableMenu.row()
+        tableMenu.add(buttonInviteFriend)
+
+
+        val tbShop = Table()
+        tbShop.setSize(SCREEN_WIDTH.toFloat(), SCREEN_HEIGHT - tableTitle.height)
+        tbShop.background = Assets.backgroundShop
+        tbShop.pad(25f, 5f, 15f, 5f)
 
         // Contenedor
-        table = new Table();
-        table.defaults().expand().fill().padLeft(10).padRight(20).padBottom(10);
+        table = Table()
+        table.defaults().expand().fill().padLeft(10f).padRight(20f).padBottom(10f)
 
-        scroll = new ScrollPane(table, new ScrollPaneStyle(null, null, null, null, null));
-        scroll.setFadeScrollBars(false);
-        scroll.setSize(SCREEN_WIDTH - tableMenu.getWidth(), 420);
-        scroll.setPosition(tableMenu.getWidth() + 1, 0);
-        scroll.setVariableSizeKnobs(false);
+        scroll = ScrollPane(table, ScrollPaneStyle(null, null, null, null, null))
+        scroll.fadeScrollBars = false
+        scroll.setSize(SCREEN_WIDTH - tableMenu.width, 420f)
+        scroll.setPosition(tableMenu.width + 1, 0f)
+        scroll.variableSizeKnobs = false
 
-        tbShop.add(tableMenu).expandY().width(122);
-        tbShop.add(scroll).expand().fill();
+        tbShop.add(tableMenu).expandY().width(122f)
+        tbShop.add(scroll).expand().fill()
 
-        stage.addActor(tableTitle);
-        stage.addActor(tbShop);
-
-
-        if (game.arrayOfPersons != null)
-            updateLeaderboard();
+        stage.addActor(tableTitle)
+        stage.addActor(tbShop)
 
 
-        buttonLeaderboard.setChecked(true);
+        if (game.arrayOfPersons != null) updateLeaderboard()
 
 
+        buttonLeaderboard!!.isChecked = true
     }
 
-    void initButtons() {
-        buttonLeaderboard = new Button(Assets.buttonShop, Assets.buttonShopPressed, Assets.buttonShopPressed);
-        buttonFacebook = new Button(Assets.buttonFacebook, Assets.buttonFacebookPressed, Assets.buttonFacebookPressed);
-        buttonGoogle = new Button(Assets.buttonAchievement, Assets.buttonAchievementPressed, Assets.buttonLeaderboardPressed);
-        buttonInviteFriend = new Button(Assets.buttonSettings, Assets.buttonSettingsPressed, Assets.buttonLeaderboardPressed);
+    fun initButtons() {
+        buttonLeaderboard = Button(Assets.buttonShop, Assets.buttonShopPressed, Assets.buttonShopPressed)
+        buttonFacebook = Button(Assets.buttonFacebook, Assets.buttonFacebookPressed, Assets.buttonFacebookPressed)
+        buttonGoogle =
+            Button(Assets.buttonAchievement, Assets.buttonAchievementPressed, Assets.buttonLeaderboardPressed)
+        buttonInviteFriend =
+            Button(Assets.buttonSettings, Assets.buttonSettingsPressed, Assets.buttonLeaderboardPressed)
 
-        buttonLeaderboard.addListener(new ClickListener() {
+        buttonLeaderboard!!.addListener(object : ClickListener() {
+        })
 
-        });
+        buttonFacebook!!.addListener(object : ClickListener() {
+        })
 
-        buttonFacebook.addListener(new ClickListener() {
-        });
+        buttonGoogle!!.addListener(object : ClickListener() {
+        })
 
-        buttonGoogle.addListener(new ClickListener() {
-        });
+        buttonInviteFriend!!.addListener(object : ClickListener() {
+        })
 
-        buttonInviteFriend.addListener(new ClickListener() {
-        });
-
-        ButtonGroup<Button> radioGroup = new ButtonGroup<>();
-        radioGroup.add(buttonLeaderboard, buttonFacebook, buttonGoogle, buttonInviteFriend);
-
+        val radioGroup = ButtonGroup<Button>()
+        radioGroup.add(buttonLeaderboard, buttonFacebook, buttonGoogle, buttonInviteFriend)
     }
 
-    @Override
-    public void draw(float delta) {
-        Assets.backgroundNubes.render(0);
-
+    override fun draw(delta: Float) {
+        Assets.backgroundNubes.render(0f)
     }
 
-    @Override
-    public void update(float delta) {
-
+    override fun update(delta: Float) {
     }
 
-    @Override
-    public boolean keyDown(int keycode) {
-        if (keycode == Keys.ESCAPE || keycode == Keys.BACK) {
-            changeScreenWithFadeOut(GameScreen.class, game);
-            return true;
+    override fun keyDown(keycode: Int): Boolean {
+        if (keycode == Input.Keys.ESCAPE || keycode == Input.Keys.BACK) {
+            changeScreenWithFadeOut(GameScreen::class.java, game)
+            return true
         }
-        return super.keyUp(keycode);
+        return super.keyUp(keycode)
     }
 
-    public void updateLeaderboard() {
-        table.clear();
-        for (Person player : game.arrayOfPersons) {
-            LeaderBoardFrame frame = new LeaderBoardFrame(player);
-            table.add(frame).expandX().fill();
-            table.row();
+    fun updateLeaderboard() {
+        table.clear()
+        for (player in game.arrayOfPersons) {
+            val frame = LeaderBoardFrame(player!!)
+            table.add(frame).expandX().fill()
+            table.row()
         }
     }
-
 }
