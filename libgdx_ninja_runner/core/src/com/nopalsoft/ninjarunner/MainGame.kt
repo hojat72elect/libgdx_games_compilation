c2d452833d9a539b2031c2ef20b40021d2fecee7
hojat <hojat72elect@gmail.com>
2024-08-01 23:51:24 -0400

    MainGame.java -> KT

--- libgdx_ninja_runner/core/src/com/nopalsoft/ninjarunner/MainGame.kt
+++ libgdx_ninja_runner/core/src/com/nopalsoft/ninjarunner/MainGame.kt
@@ -1,41 +1,37 @@
-package com.nopalsoft.ninjarunner;
+package com.nopalsoft.ninjarunner
 
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.I18NBundle;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.nopalsoft.ninjarunner.game.GameScreen;
-import com.nopalsoft.ninjarunner.leaderboard.Person;
-import com.nopalsoft.ninjarunner.screens.Screens;
+import com.badlogic.gdx.Game
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.utils.Array
+import com.badlogic.gdx.utils.I18NBundle
+import com.badlogic.gdx.utils.viewport.StretchViewport
+import com.nopalsoft.ninjarunner.Settings.load
+import com.nopalsoft.ninjarunner.game.GameScreen
+import com.nopalsoft.ninjarunner.leaderboard.Person
+import com.nopalsoft.ninjarunner.screens.Screens
 
 /**
  * It has already been made in KT. Don't do it again.
  */
-public class MainGame extends Game {
+class MainGame : Game() {
+    @JvmField
+    var arrayOfPersons: Array<Person> = Array()
+    @JvmField
+    var stage: Stage? = null
+    @JvmField
+    var batcher: SpriteBatch? = null
+    @JvmField
+    var languages: I18NBundle? = null
 
+    override fun create() {
+        languages = I18NBundle.createBundle(Gdx.files.internal("strings/strings"))
+        batcher = SpriteBatch()
+        stage = Stage(StretchViewport(Screens.SCREEN_WIDTH.toFloat(), Screens.SCREEN_HEIGHT.toFloat()))
 
-    public Array<Person> arrayOfPersons = new Array<>();
-    public Stage stage;
-    public SpriteBatch batcher;
-    public I18NBundle languages;
-
-    public MainGame() {
-
+        load()
+        Assets.load()
+        setScreen(GameScreen(this, true))
     }
-
-    @Override
-    public void create() {
-        languages = I18NBundle.createBundle(Gdx.files.internal("strings/strings"));
-        batcher = new SpriteBatch();
-        stage = new Stage(new StretchViewport(Screens.SCREEN_WIDTH, Screens.SCREEN_HEIGHT));
-
-        Settings.load();
-        Assets.load();
-        setScreen(new GameScreen(this, true));
-
-    }
-
 }
