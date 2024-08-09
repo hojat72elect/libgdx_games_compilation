4c11802b77724bb6fe915cb688fdf18e29020f67
hojat <hojat72elect@gmail.com>
2024-08-09 02:02:52 -0400

    IOSLauncher.java -> kt

--- libgdx_ninja_runner/ios/src/com/nopalsoft/ninjarunner/IOSLauncher.kt
+++ libgdx_ninja_runner/ios/src/com/nopalsoft/ninjarunner/IOSLauncher.kt
@@ -1,25 +1,25 @@
-package com.nopalsoft.ninjarunner;
+package com.nopalsoft.ninjarunner
 
-import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
-import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration
+import org.robovm.apple.foundation.NSAutoreleasePool
+import org.robovm.apple.uikit.UIApplication
 
-import org.robovm.apple.foundation.NSAutoreleasePool;
-import org.robovm.apple.uikit.UIApplication;
-
-public class IOSLauncher extends IOSApplication.Delegate {
-
-    public static MainGame game;
-
-    public static void main(String[] argv) {
-        NSAutoreleasePool pool = new NSAutoreleasePool();
-        UIApplication.main(argv, null, IOSLauncher.class);
-        pool.close();
+class IOSLauncher : IOSApplication.Delegate() {
+    override fun createApplication(): IOSApplication {
+        val config = IOSApplicationConfiguration()
+        game = MainGame()
+        return IOSApplication(game, config)
     }
 
-    @Override
-    protected IOSApplication createApplication() {
-        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        game = new MainGame();
-        return new IOSApplication(game, config);
+    companion object {
+        var game: MainGame? = null
+
+        @JvmStatic
+        fun main(argv: Array<String>) {
+            val pool = NSAutoreleasePool()
+            UIApplication.main<UIApplication, IOSLauncher>(argv, null, IOSLauncher::class.java)
+            pool.close()
+        }
     }
 }
